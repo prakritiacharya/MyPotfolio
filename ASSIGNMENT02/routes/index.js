a8d3bb110a5b15457ff94e2e752c99c7548a7314
Prakriti Acharya <prakriti@acharya.com>
2025-08-07 21:23:44 -0400

    created different views

--- ASSIGNMENT02/routes/index.js
+++ ASSIGNMENT02/routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/', function(req, res, next) {
+  res.render('expenses', { title: 'Express' });
+});
+
+
 module.exports = router;
