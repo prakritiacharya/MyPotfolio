5cebb8957df7436be9a16cc0c930cd9e25510cc6
Prakriti Acharya <prakriti@acharya.com>
2025-08-12 21:59:10 -0400

    Added login and register views

--- ASSIGNMENT02/app.js
+++ ASSIGNMENT02/app.js
@@ -12,17 +12,42 @@ var configs = require("./configs/globals");
 var mongoose=require("mongoose");
 const { config } = require('process');
 
+
+// import passort and session modules
+var session = require("express-session");
+var passport = require("passport");
+// Import User model so we can configure passport with the plm functionality
+var User = require("./models/user"); // ./ means root directory
+
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-
+ //Middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// initialize session middleware
+app.use(session({
+  secret: "ProjectTrackerSecret123",
+  resave: false,
+  saveUninitialized: false,
+}));
+
+//initialize passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Initialize Basic Authentication Strategy
+passport.use(User.createStrategy()); // Out-of-the-box strategy from passport-local-mongoose
+// Configure serialization and deserialization of user instances to support persistent login sessions (read/write to session)
+passport.serializeUser(User.serializeUser()); // Serialize user instance to session
+passport.deserializeUser(User.deserializeUser()); // Deserialize user instance from session
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
