74fd54d76fc1a5d3921130f09920a47a472a1308
Prakriti Acharya <prakriti@acharya.com>
2025-08-12 23:15:43 -0400

    Route the login and register page

--- ASSIGNMENT02/routes/index.js
+++ ASSIGNMENT02/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Expense = require('../models/Expense'); // your mongoose model
+var passport = require("passport");
 
 router.get('/', async (req, res) => {
   try {
@@ -49,5 +50,49 @@ router.get('/', async (req, res) => {
     res.status(500).send('Error loading dashboard');
   }
 });
+
+// GET /login
+router.get("/login", function (req, res, next) {
+  res.render("login", { title: "Login" });
+});
+
+// POST /login
+router.post("/login", passport.authenticate(
+    "local", // strategy name
+    {
+      // options
+      successRedirect: "/projects", // redirect to projects on success
+      failureRedirect: "/login", // redirect to login on failure
+      failureMessage: "Invalid Credentials", // message on failure stored in session object
+    }
+  )
+);
+
+// GET /register
+router.get("/register", function (req, res, next) {
+  res.render("register", { title: "Register" });
+});
+
+// POST /register
+router.post("/register", function (req, res, next) {
+  // Create a new user based on input from the form
+  // Call the User model and pass three parameters: user data, password, and callback
+  User.register(
+    new User({ username: req.body.username }), // new user data
+    req.body.password, // password from the form
+    function (error, newUser) {
+      if (error) {
+        console.log(error);
+        return res.redirect("/register");
+      } else {
+        // log user in and redirect to projects
+        req.login(newUser, (err) => {
+          res.redirect("/projects");
+        });
+      }
+    }
+  );
+});
+
 
 module.exports = router;
